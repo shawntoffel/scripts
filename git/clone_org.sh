58f820ef68dd8a68cbe906ccf7c956d97a91bfd8
stoffel <stoffel@commercesync.com>
2019-05-14 11:18:43 -0600

    added comment

--- git/clone_org.sh
+++ git/clone_org.sh
@@ -1,3 +1,5 @@
+# clone all repos in an organization
+
 ACCESS_TOKEN = ""
 ORG = ""
 curl -s https://api.github.com/orgs/$ORG/repos?access_token=$ACCESS_TOKEN | grep -e ssh_url | cut -d \" -f 4 | xargs -L1 git clone --recursive
