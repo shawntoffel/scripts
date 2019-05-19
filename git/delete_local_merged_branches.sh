4b3fd356780473be4367becbef3df18591804518
Shawn Toffel <shawn.toffel@gmail.com>
2019-05-19 19:19:23 +0000

    delete_local_branches -> delete_local_merged_branches

--- git/delete_local_merged_branches.sh
+++ git/delete_local_merged_branches.sh
@@ -1,6 +1,8 @@
  # delete all local branches that have been merged. Also prunes stale local remote refs.
+echo "deleting local merged branches..."
 for b in `git branch --merged | grep -v \*`; do
     git branch -d $b
 done
 
+echo "pruning stale remote refs"
 git remote prune origin
