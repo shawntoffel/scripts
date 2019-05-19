8a5d4f94b0a85c3d12a98bd75b6b92cbb0a10e78
Shawn Toffel <shawn.toffel@gmail.com>
2019-05-19 19:48:24 +0000

    simplify recent

--- git/recent.sh
+++ git/recent.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 # print local branches ordered by date of last commit
 for k in $(git branch | perl -pe s/^..//); do
-    echo -e "$(git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k" -- | head -n 1)"\\t"$k"
+    echo "$(git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k" -- | head -n 1)" "$k"
 done | sort -r
