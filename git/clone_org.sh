fb7bbb94c4127e85c639488eb03b4fbc9356c9dd
Shawn Toffel <shawn.toffel@gmail.com>
2019-08-29 12:21:06 -0600

    Update clone_org.sh

--- git/clone_org.sh
+++ git/clone_org.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-# clone all repos in an organization
-ACCESS_TOKEN=""
-ORG=""
-curl -s https://api.github.com/orgs/$ORG/repos?access_token=$ACCESS_TOKEN | grep -e ssh_url | cut -d \" -f 4 | xargs -L1 git clone --recursive
+#-----------------------------------------------------------------------------
+# Input Parameters
+#
+# access_token 
+# - Your GitHub personal access token with repo scope
+#
+# organization
+# - String value indicating the GitHub organization name
+#-----------------------------------------------------------------------------
+
+access_token=$1
+organization=${2:-""} 
+
+if [ -z "$access_token" ]
+  then
+    echo "access_token is required"
+    exit 1
+fi
+
+curl -s https://api.github.com/orgs/$organization/repos?access_token=$access_token | grep -e ssh_url | cut -d \" -f 4 | xargs -L1 git clone --recursive
