d61100fa5f4a91d705eb030ad3eda875838bfa3d
Shawn Toffel <shawn.toffel@gmail.com>
2020-06-22 15:33:38 -0600

    Handle trailing slash in param & don't auto commit

--- git/remove_submodule.sh
+++ git/remove_submodule.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-submodule="$1"
+submodule=${1%/}
 
 git rm "$submodule"
 rm -rf ".git/modules/$submodule"
-git config -f ".git/config" --remove-section "submodule.$submodule" 2> /dev/null
-
-git commit -m "Removed $submodule submodule"
+git config -f ".git/config" --remove-section "submodule.$submodule"
